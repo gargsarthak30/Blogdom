35ea0c81b17d5b462734e619771194fc478b9323
Sarthak Garg <gargsarthak30@gmail.com>
2016-09-09 18:43:29 +0530

    Final steps for heroku

--- blogdom/settings.py
+++ blogdom/settings.py
@@ -19,8 +19,8 @@ DEBUG = TEMPLATE_DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-# ALLOWED_HOSTS = ["https://blogdom.herokuapp.com/"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://blogdom.herokuapp.com/"]
+
 
 # Application definition
 
