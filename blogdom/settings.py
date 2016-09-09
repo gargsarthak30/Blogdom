bae5e40a3d87f7428ef7914e92bf20eb43bd2197
Sarthak Garg <gargsarthak30@gmail.com>
2016-09-09 19:07:14 +0530

    Removed static tag from user accounts

--- blogdom/settings.py
+++ blogdom/settings.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse_lazy
 import os
-
+from whitenoise import WhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
