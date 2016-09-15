c8b85e98cc2395fa01be2a5a973a57f05261d436
Sarthak Garg <gargsarthak30@gmail.com>
2016-09-15 19:40:08 +0530

    staticfiles link again

--- blogdom/urls.py
+++ blogdom/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
     url(r'^user_account/', include('user_account.urls')),
 ]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
