7d5d69eb464ba00e444410b0aa0b226f3fe02f65
Sarthak Garg <gargsarthak30@gmail.com>
2016-09-15 14:08:14 +0530

    Removed unnecessary files, Updated database

--- blogs/models.py
+++ blogs/models.py
@@ -60,7 +60,6 @@ class Post(models.Model):
     blogger = models.ForeignKey(UserBlogdom, on_delete=models.CASCADE)
     heading = models.CharField(max_length=150)
     blog_content = models.TextField()
-    public = models.BooleanField(default=True)
     upvotes = models.IntegerField(default=0)
     time = models.DateTimeField(editable=False, default=timezone.now)
     image = models.ImageField(blank=True, null=True, upload_to=content_post_image)
