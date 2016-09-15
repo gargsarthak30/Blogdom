9e732454f582dd020db92fc5f8bdb05ef4e2353a
Sarthak Garg <gargsarthak30@gmail.com>
2016-09-15 18:34:27 +0530

    Debug ON

--- blogdom/settings.py
+++ blogdom/settings.py
@@ -13,7 +13,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-rf$^(3fzvydbwi*b+t^jp*8v_%*e(t%d4$ul!ywey8k=q$5+m'
 
-DEBUG = False
+DEBUG = True
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -56,7 +56,7 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-            'debug': False,
+            'debug': True,
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
