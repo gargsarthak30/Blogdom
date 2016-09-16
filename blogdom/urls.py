d310055513eeaf4ef193372ece70e4562d8ddc60
Sarthak Garg <gargsarthak30@gmail.com>
2016-09-16 10:31:23 +0530

    Redirect view for home

--- blogdom/urls.py
+++ blogdom/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.Index, name = 'home'),
+    url(r'^$', RedirectView.as_view(url='/blogs/')),
     url(r'^blogs/', include('blogs.urls')),
     url(r'^user_account/', include('user_account.urls')),
 ]
