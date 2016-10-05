e8c2cf662637d118b8adc169dc18769ccf03831f
Sarthak Garg <gargsarthak30@gmail.com>
2016-10-06 01:17:26 +0530

    Revert "master branch"
    This reverts commit eca91e72bb5d336646e8b8691fa7e78c5aa56cc0.

---
+++ blogs/urls.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.conf.urls import url
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
+urlpatterns = [
+    url(r'^logout_user/', views.LogoutUser, name='logout'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^new_post/$', login_required(views.PostCreateView.as_view()), name='new_post'),
+    url(r'^content/(?P<id>[0-9]+)', views.load_blog_content, name= "load_blog_content"),
+    url(r'^(?P<pk>[-\w.]+)/$', views.UserProfile, name='user_profile'),
+    url(r'^profile/edit/', views.UserBlogdomUpdate, name='user_profile_edit'),
+    url(r'^post/edit/(?P<pk>[0-9]+)/', login_required(views.PostUpdate.as_view()), name="post_edit"),
+    url(r'upvote/(?P<post_id>[0-9]+)/', views.post_upvote, name='post_upvote'),
+    url(r'^data/users/$', views.UserBlogdom_List.as_view(), name="users-api"),
+    url(r'^data/posts/$', views.Post_List.as_view(), name="posts-api"),
+    url(r'^data/posts/(?P<username>[\w-]+)/$', views.Post_Detail.as_view(), name="posts-api-specific"),
+    url(r'^data/users/(?P<user__username>[\w-]+)/$', views.UserBlogdom_Detail.as_view(), name="users-api-specific"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ user_account/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class PasswordResetRequestForm(forms.Form):
+    email_or_username = forms.CharField(label=("Email or Username"), max_length=254, widget=forms.TextInput(attrs={'class':'form-control'}))
+
+
+class SetPasswordForm(forms.Form):
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match!"
+    }
+    new_password1 = forms.CharField(label="New Passsword", widget=forms.PasswordInput)
+    new_password2 = forms.CharField(label="New Password Confirmation", widget=forms.PasswordInput)
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(self.error_messages['password_mismatch'], code='password_mismatch',)
+        return password2
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'email')
+
+    def save(self, commit = True):
+        User = super(RegistrationForm, self).save(commit=False)
+        User.email = self.cleaned_data["email"]
+
+        if commit:
+            User.save()
+
+        return User
+
+
+
