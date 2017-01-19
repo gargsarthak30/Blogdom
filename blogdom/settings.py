6667d881aa0b073152bc594cec5f83d4f293b94b
Sarthak Garg <gargsarthak30@gmail.com>
2017-01-20 00:23:00 +0530

    Cloudinary support (Major Change)

--- blogdom/settings.py
+++ blogdom/settings.py
@@ -2,6 +2,9 @@ from django.core.urlresolvers import reverse_lazy
 import os
 from whitenoise import WhiteNoise
 import socket
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -12,7 +15,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-rf$^(3fzvydbwi*b+t^jp*8v_%*e(t%d4$ul!ywey8k=q$5+m'
+SECRET_KEY = '#################################3'
 
 if socket.gethostname() == 'SARTHAK':
     DEBUG = True
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
     'blogs.apps.BlogsConfig',
     'user_account.apps.UserAccountConfig',
 ]
@@ -71,7 +75,13 @@ TEMPLATES = [
     },
 ]
 
+# WSGI_APPLICATION = 'blogdom.wsgi.application'
 WSGI_APPLICATION = 'blogdom.wsgi.application'
+cloudinary.config(
+    cloud_name="blogdom",
+    api_key="###########",
+    api_secret="################"
+)
 
 
 # Database
@@ -82,9 +92,9 @@ if not DEBUG:
         'default': {
             'HOST': 'ec2-54-243-202-110.compute-1.amazonaws.com',
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': '',
-            'USER': '',
-            'PASSWORD': '',
+            'NAME': '########################',
+            'USER': '################',
+            'PASSWORD': '###################',
         }
     }
 else:
@@ -142,7 +152,7 @@ LOGIN_URL = reverse_lazy('user_account.views.user_login')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join((BASE_DIR), 'static')
 STATIC_URL = '/static/'
 
--- blogdom/urls.py
+++ blogdom/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^blogs/', include('blogs.urls')),
     url(r'^user_account/', include('user_account.urls')),
 ]
+
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
